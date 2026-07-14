840d45ffc650c0d3f7f04b0cc88364f1aaaa7788
Kuu Kuu <ivynora756@gmail.com>
2026-07-14 15:24:32 +0630

    setting

--- trustyshop/settings.py
+++ trustyshop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'user',
 ]
 
+AUTH_USER_MODEL = 'welcome.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
