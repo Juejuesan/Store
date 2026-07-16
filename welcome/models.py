c15d5186ff80be403bb24cbcd410edde80bf0263
Ye Min Aung <yelminaung816@gmail.com>
2026-07-16 21:09:55 +0630

    post create done

--- welcome/models.py
+++ welcome/models.py
@@ -6,54 +6,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
+from user.models import User
 
-class User(AbstractUser):
-
-    ROLE_CHOICES = (
-        ('buyer', 'Buyer'),
-        ('seller', 'Seller'),
-        ('both', 'Buyer & Seller'),
-    )
-
-    phone = models.CharField(
-        max_length=20,
-        blank=True
-    )
-
-    profile_image = models.ImageField(
-        upload_to='profiles/',
-        blank=True,
-        null=True
-    )
-
-    address = models.TextField(
-        blank=True
-    )
-
-    balance = models.DecimalField(
-        max_digits=12,
-        decimal_places=2,
-        default=0
-    )
-
-    role = models.CharField(
-        max_length=10,
-        choices=ROLE_CHOICES,
-        default='both'
-    )
-
-    trust_score = models.PositiveIntegerField(
-        default=100
-    )
-
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-
-    def __str__(self):
-        return self.username
 
 class Product(models.Model):
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
