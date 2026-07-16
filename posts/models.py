a5c43bc6f661cd21c8e538b7fdd7010b4da319f6
Ye Min Aung <yelminaung816@gmail.com>
2026-07-16 22:09:01 +0630

    error in user

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,6 @@
+from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 from django.db import models
-
-from user.models import User
 
 
 class Category(models.Model):
--- user/models.py
+++ user/models.py
@@ -1,17 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class User(models.Model):
-    name = models.CharField(max_length=30,unique=True)
-    profile = models.ImageField(upload_to='images/')
-    phNumber = models.CharField(max_length=11,unique=True)
-    email = models.EmailField(max_length=30,unique=True)
-    password = models.CharField(max_length=20)
-    address = models.TextField(max_length=100)
-    gender = models.CharField(max_length=10)
-    balance = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
+class Customer(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.jpg', blank=True, null=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.user.username}'s Profile"
 
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
