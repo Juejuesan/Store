7a8c8410508e162fd9dc8f79ca8412e2e9bc9d97
Jue Jue San <sanjuejue79@gmail.com>
2026-07-19 16:05:36 +0630

    Merging with dev01

--- user/models.py
+++ user/models.py
@@ -9,11 +9,17 @@ class Profile(models.Model):
         ('F', 'Female'),
     ]
 
+    STATUS = [
+        ( 'Approved', 'approved'),
+        ( 'Banned', 'banned'),
+    ]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+    address = models.TextField()
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
     profile_pic = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.jpg', blank=True, null=True)
+    status = models.CharField(max_length=10, choices=STATUS,default='Approved')
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
