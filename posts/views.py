fbb08b91fdbc4deff6341d0bebe5c384c35d768d
Ye Min Aung <yelminaung816@gmail.com>
2026-07-20 16:05:30 +0630

    test user create and fix post create

--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,37 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from posts.form import PostForm
 from posts.models import PostImage
+from user.models import Profile
 
+
+@login_required()
 def createPost(request):
+    user = Profile.objects.get(user=request.user)
 
-    if request.method == "POST":
-        form = PostForm(request.POST, request.FILES)
+    if user.STATUS == 'banned' :
+        messages.error(request, "Your account is not approved yet.")
+        return redirect('home')
+    else:
 
-        if form.is_valid():
+        if request.method == "POST":
+         form = PostForm(request.POST, request.FILES)
+
+         if form.is_valid():
             post=form.save(commit=False)
             post.user = request.user
 
             post.save()
 
             images = request.FILES.getlist('images')
+
+            if len(images) == 0:
+                messages.error(request, "Please upload at least one photo.")
+                return render(request, "posts/createPost.html", {"form": form})
+
+            if len(images) > 5:
+                messages.error(request, "You can upload a maximum of 5 photos.")
 
             for img in images:
                 PostImage.objects.create(
@@ -23,7 +40,7 @@ def createPost(request):
                 )
             return redirect("home")
 
-    else:
-        form = PostForm()
+        else:
+            form = PostForm()
 
-    return render(request, "posts/createPost.html", {"form": form})
+        return render(request, "posts/createPost.html", {"form": form})
