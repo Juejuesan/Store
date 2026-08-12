0ce09f8e7c8907a31f0cc4b69a88d5e50e2044f4
Ye Min Aung <yelminaung816@gmail.com>
2026-08-13 01:39:38 +0630

    change url

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('createPost/', views.create_post, name='create_post'),
+    path('createPost/', views.create_post, name='createPost'),
     path('get-category-sizes/<int:category_id>/', views.get_category_sizes, name='get_category_sizes'),
 ]
