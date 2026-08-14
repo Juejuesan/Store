a8c29257ddadda8b0403b13f77e96421ccca99d0
Jue Jue San <sanjuejue79@gmail.com>
2026-08-14 22:25:06 +0630

    migrations files 0003 pass

---
+++ posts/migrations/0003_auto_20260813_0001.py
@@ -0,0 +1,30 @@
+from django.db import migrations
+
+
+def add_perfume(apps, schema_editor):
+    Category = apps.get_model('posts', 'Category')
+
+    Category.objects.get_or_create(
+        name='Perfume',
+        defaults={
+            'size_type': 'none',
+            'size_label': None,
+        }
+    )
+
+
+def remove_perfume(apps, schema_editor):
+    Category = apps.get_model('posts', 'Category')
+
+    Category.objects.filter(name='Perfume').delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('posts', '0002_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_perfume, remove_perfume),
+    ]
