078854105ce4d9cb9c92824d01f37fa70a6ede14
Jue Jue San <sanjuejue79@gmail.com>
2026-08-24 01:29:52 +0630

    Finished post filter

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,28 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
 
-    # If your coworker already made a home view function inside home/views.py,
-    # you can link it here. For now, this empty path satisfies Django.
-    # path('', views.home_view, name='home'),
-    path('', views.home, name='home'),
-    path('viewdetail/<int:post_id>/', views.viewdetail, name='viewdetail'),
-    path("createPost/", views.createPost, name="createPost")
+    # =====================================================
+    # HOME
+    # =====================================================
+
+    path(
+        '',
+        views.home,
+        name='home'
+    ),
+
+
+    # =====================================================
+    # PRODUCT DETAIL
+    # =====================================================
+
+    path(
+        'viewdetail/<int:post_id>/',
+        views.viewdetail,
+        name='viewdetail'
+    ),
+
 ]
--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,42 @@ from . import views
 
 urlpatterns = [
 
-    path(  "createPost/",views.create_post,name="createPost"),
-    path("my-posts/",views.my_posts,name="my_posts"),
-    path("get-category-sizes/<int:category_id>/",views.get_category_sizes,name="get_category_sizes"),
+    # =====================================================
+    # CREATE POST
+    # =====================================================
+
+    path(
+        "createPost/",
+        views.create_post,
+        name="createPost"
+    ),
+
+
+    # =====================================================
+    # MY POSTS
+    # =====================================================
+
+    path(
+        "my-posts/",
+        views.my_posts,
+        name="my_posts"
+    ),
+
+
+    # =====================================================
+    # GET CATEGORY SIZES
+    # =====================================================
+
+    path(
+        "get-category-sizes/<int:category_id>/",
+        views.get_category_sizes,
+        name="get_category_sizes"
+    ),
+
+path(
+    "editPost/<int:post_id>/",
+    views.edit_post,
+    name="editPost"
+),
+
 ]
